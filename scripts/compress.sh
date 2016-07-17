920ca7d6082752e15d1bc1ebe278da7ec060d95e
Raphael Amorim <rapha850@gmail.com>
2016-07-17 18:40:31 -0300

    uppercase vars name and fix script description

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# name: compress-css
-# description: rewrite CSS without any white spaces and create <file-name>.min.css
+# name: compress
+# description: rewrite file without any white spaces and create <file-name>.min.css
 
-SCRIPT_NAME="compress-css"
+SCRIPT_NAME="compress"
 
 if [ $# -gt 0 ]; then
-    it=1
-    done=0
+    IT=1
+    DONE=0
 
     sp='/-\|'
     printf ' '
-    until [ $it -gt 100 ]
+    until [ $IT -gt 100 ]
     do
       printf '\b%.1s' "$sp"
       sp=${sp#?}${sp%???}
-      if [ $done -lt 1 ]; then
+      if [ $DONE -lt 1 ]; then
         FILE=$(cat "$1")
         FILE=$(sed -e '/^\(\s*\)\/\//d' <<< $FILE)
         FILE=$(sed -e 's/^[ \t]*//g; s/[ \t]*$//g;' <<< $FILE)
         FILE=$(sed -e ':a;N;$!ba;s/\n/ /g' <<< $FILE)
 
-        done=1
+        DONE=1
       fi
-      it=`expr $it + 1`
+      IT=`expr $IT + 1`
     done
 else
-    printf "${SCRIPT_NAME} : CSS file path is needed"
+    printf "${SCRIPT_NAME} : file path is needed"
 fi
