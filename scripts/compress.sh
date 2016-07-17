85914dd3fafcf98cd83daa335a773367cb305b76
Raphael Amorim <rapha850@gmail.com>
2016-07-17 18:38:40 -0300

    apply loader and fix bash link

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # name: compress-css
 # description: rewrite CSS without any white spaces and create <file-name>.min.css
@@ -6,9 +6,25 @@
 SCRIPT_NAME="compress-css"
 
 if [ $# -gt 0 ]; then
-    echo $1
-    echo $(cat $1 | tr -d " \t\n\r")
+    it=1
+    done=0
 
+    sp='/-\|'
+    printf ' '
+    until [ $it -gt 100 ]
+    do
+      printf '\b%.1s' "$sp"
+      sp=${sp#?}${sp%???}
+      if [ $done -lt 1 ]; then
+        FILE=$(cat "$1")
+        FILE=$(sed -e '/^\(\s*\)\/\//d' <<< $FILE)
+        FILE=$(sed -e 's/^[ \t]*//g; s/[ \t]*$//g;' <<< $FILE)
+        FILE=$(sed -e ':a;N;$!ba;s/\n/ /g' <<< $FILE)
+
+        done=1
+      fi
+      it=`expr $it + 1`
+    done
 else
     printf "${SCRIPT_NAME} : CSS file path is needed"
 fi
